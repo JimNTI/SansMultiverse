1f09384d4bb72ae05597397fe4b0d913903a99e6
JimNTI <jim.palo@elev.ga.ntig.se>
2025-03-22 22:07:01 +0100

    anoter push

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+document.querySelector(".categories-button").addEventListener("click", showCategory);
+
+function showCategory(){
+    let categoryOpener = document.querySelector(".categoriesMenu");
+    categoryOpener.classList.toggle("visible");
+}
+
+document.querySelector(".teleport-button").addEventListener("click", teleport)
+
+function teleport(){
+    let teleportOpener = document.querySelector(".teleportMenu");
+    teleportOpener.classList.toggle("visible");
+    let clickAnimation = document.querySelector(".teleport-button")
+    clickAnimation.classList.toggle("lighten");
+    let activateSound = document.querySelector("#blipblip")
+    activateSound.play();
+    }
+
